f3263332df39b597199a07984cda9338894c27cf
najm-choueiry <nachoueiry@outlook.com>
2023-02-25 13:41:49 +0200

    final

--- game.js
+++ game.js
@@ -1,63 +1,95 @@
 window.onload = function (){
     
     var status = document.getElementById("status")
-
     var game = document.getElementById("game")
-
     var start = document.getElementById("start")
-
     var boundary = document.getElementsByClassName("boundary")
-
     var end = document.getElementById("end")
+    var counting = document.getElementById('counter')
 
-    var game_start = false
+    var game_started = false
+    var game_finished = false
 
-    var counter = 0
-
-
-    start.addEventListener("mouseover", function(){
-        status.innerHTML = "the game has begun"
-        var game_start = true
-
-        for (var i=0; i<boundary.length ; i++) { 
-             boundary[i].addEventListener("mouseover", function(){
-                 for (let i = 0; i < boundary.length; i++) {
-                    boundary[i].classList.add('youlose')
-                 }
-                 
-                 
-                })
-             }
-
-    })    
-
-
-
-
-
-        start.addEventListener('click', function(){
-            counter -=10;
-            for (let i = 0; i < boundary.length; i++) {
-                boundary[i].classList.remove('youlose')
-            }
-
-            status.innerHTML = "The game has reset"
-            var game_start=false
-        })
+    var score = 0
+    // starting the game
+    start.addEventListener('mouseenter', start_the_game)
+    function start_the_game() {
+        status.innerHTML = "the game has begun";
+        game_started = true;
+        // game_finished = false;
+        console.log(game_started)
+        }
         
 
+ 
+    // loosing the game
+    for (let i=0; i<boundary.length; i++) {
+    boundary[i].addEventListener("mouseover", you_lose)
+    }
+    function you_lose() {
+    if (game_started && !game_finished) {
+            for (let i=0; i<boundary.length; i++) {
+        boundary[i].classList.add('youlose');
+
+    }   
+    status.innerHTML = "you lose";
+    score -=10
+    counting.innerHTML = "Counter " + score
+    game_finished=false
+
+    
+    }
+    else if (game_started && game_finished==true ) {
+        for (let i=0; i<boundary.length; i++) {
+            boundary[i].classList.remove('youlose');
+        } 
+        status.innerHTML = "You already won !"
+    }
+    }
+    
+
+    // outisde the box
+    game.addEventListener('mouseleave', outside_the_box)
+    function outside_the_box(){ 
+    for (let i=0; i<game.length; i++) {
+        game[i].classList.add('youlose');
+    }
+    status.innerHTML = "Stick to the box"
+}
+
+    // ending the game (win)
+//     end.addEventListener('mouseover', end_the_game)
+      
+//     function end_the_game() {
+//         if (game_started ==true  && game_finished == false) {
+//         status.innerHTML = "The game has ended. Please restart"
+//         game_finished=true;
+//     }
+// }
 
 
-    if (game_start == true)
-    end.addEventListener('mouseover', function ( ) {
-        for (let i = 0; i < boundary.length; i++) {
-            boundary[i].classList.remove('youlose')
-        }
-        status.innerHTML = "You Won !"
-    })
+    // restarting the game
+    start.addEventListener("click", restarting) 
+    function restarting () {
+        for (let i=0; i<boundary.length; i++) {
+            boundary[i].classList.remove('youlose');
+        } 
+        status.innerHTML = "the game has restarted"
+        counting = 0
+        game_finished=false
+    }
 
+    //ending the game 
 
+    end.addEventListener("mouseover", end_the_game)
+    function end_the_game() {
+        if (game_started == true && game_finished ==false ) {
+            status.innerHTML = " YOU win ! go back to S to continue"
+                // i need to reset all the previous functions and addeventlisteners 
+        game_finished = true;
+    }
+    score +=5
+    counting.innerHTML = "Counter " + score
 
-
-
+}
 }
